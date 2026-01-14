7869843bfbd5be725e3c3128abe81873cb5aa18c
sitbacrelaxGT <tapo.greg@gmail.com>
2026-01-13 22:35:11 -0700

    refactor: replace SVG logo with image for improved rendering
    Signed-off-by: sitbacrelaxGT <tapo.greg@gmail.com>

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,17 +6,10 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({ className = "w-12 h-12" }) => {
   return (
-    <svg 
-      viewBox="0 0 200 200" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg" 
+    <img 
+      src="/assets/Images/Icon Logo_Color Transparent Background enlarged.png"
+      alt="Percon Solutions Logo"
       className={className}
-    >
-      {/* House/Chevron shape - upward pointing, filled with cyan */}
-      <path 
-        d="M100 30 L170 85 L170 175 L30 175 L30 85 Z" 
-        fill="#22d3ee"
-      />
-    </svg>
+    />
   );
 };
